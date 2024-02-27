603cc0bff8f79cbe9da90e89a7b11c8af22312b5
Pritam <pritammagdum24@gmail.com>
2024-02-27 22:43:55 +0530

    Connected MongoDB Database with the help of Mongoose Package

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hey Pritam");
+const express = require("express");
+const server = express();
+const mongoose = require("mongoose");
+
+server.get("/", (req, res) => {
+  res.json({ status: "success" });
+});
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+  console.log("MongoDB Database Connected");
+}
+
+const PORT = 8080;
+server.listen(PORT, () => {
+  console.log(`Server Started at ${PORT}`);
+});
