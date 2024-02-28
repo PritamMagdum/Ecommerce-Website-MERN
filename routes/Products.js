28d5083025f80049307b5e9ae0b9fcaaf14bbd95
Pritam <pritammagdum24@gmail.com>
2024-02-28 17:35:14 +0530

    Added API or Endpoint For Updating Product By ID

--- routes/Products.js
+++ routes/Products.js
@@ -1,9 +1,18 @@
 const express = require("express");
-const { ceateProduct, fetchAllProducts } = require("../controller/Product");
+const {
+  ceateProduct,
+  fetchAllProducts,
+  fetchProductById,
+  updateProduct,
+} = require("../controller/Product");
 
 const router = express.Router();
 
 // /produts is already added in the base path so no need to add
-router.post("/", ceateProduct).get("/", fetchAllProducts);
+router
+  .post("/", ceateProduct)
+  .get("/", fetchAllProducts)
+  .get("/:id", fetchProductById)
+  .patch("/:id", updateProduct);
 
 exports.router = router;
